3e01819c8ea63967e614c04effc6e3fcf874c26e
Vinicius Rocca <vinicius.erocca@gmail.com>
2025-09-05 20:32:39 -0300

    step 4 commit (connection to databse)

--- week01/project1/server.js
+++ week01/project1/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongodb = require('./data/database.js');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'))
+app.use('/', require('./routes'));
 
-app.listen(port, () => {console.log(`Running on port ${port}`)})
+mongodb.initDb((err) => {
+    if(err){
+        console.log(err);
+    } else {
+        app.listen(port, () => {console.log(`Database is listening and note is running on port ${port}`)})
+    }
+});
