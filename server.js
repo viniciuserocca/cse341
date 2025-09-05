7d2c8e16ff8d884edb28a9e83aad241a30f4e3ca
Vinicius Rocca <vinicius.erocca@gmail.com>
2025-09-04 23:59:23 -0300

    Adding lesson 1 assignment code

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Vinicius');
+});
+
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port' + (process.env.port || 3000));
+
