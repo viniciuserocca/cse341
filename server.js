8fdef88c52a5b323deaea1a3204772ba6d04e875
Vinicius Rocca <vinicius.erocca@gmail.com>
2025-09-09 00:18:42 -0300

    step 1 week 2 added

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongodb = require('./data/database.js');
 const app = express();
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
     if(err){
         console.log(err);
     } else {
-        app.listen(port, () => {console.log(`Database is listening and note is running on port ${port}`)})
+        app.listen(port, () => {console.log(`Database is listening and node is running on port ${port}`)})
     }
 });
