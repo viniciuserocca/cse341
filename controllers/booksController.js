ec4df2f41a0010e2b486a4597e5841a98f7bcd22
Vinicius Rocca <vinicius.erocca@gmail.com>
2025-09-25 13:26:25 -0300

    try and catch corrected

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -79,67 +79,82 @@ const getSingle = async (req, res) => {
 
 // Create a single book
 const createBook = async (req, res) => {
-  const book = {
-    title: req.body.title,
-    author: req.body.author,
-    genre: req.body.genre,
-    publicationDate: req.body.publicationDate,
-    isbn: req.body.isbn,
-    summary: req.body.summary,
-    createdBy: null,
-    createdAt: new Date(),
-    updatedBy: null,
-    updatedAt: null
-  };
-  const response = await mongodb.getDatabase().db().collection('books').insertOne(book);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the book.');
+  try {
+    const book = {
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      publicationDate: req.body.publicationDate,
+      isbn: req.body.isbn,
+      summary: req.body.summary,
+      createdBy: null,
+      createdAt: new Date(),
+      updatedBy: null,
+      updatedAt: null
+    };
+
+    const response = await mongodb.getDatabase().db().collection('books').insertOne(book);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while creating the book.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
 // Update a single book
 const updateBook = async (req, res) => {
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Must use a valid book id to update a book.');
-  }
-  const userId = new ObjectId(req.params.id);
-  const book = {
-    title: req.body.title,
-    author: req.body.author,
-    genre: req.body.genre,
-    publicationDate: req.body.publicationDate,
-    isbn: req.body.isbn,
-    summary: req.body.summary,
-    //updatedBy: null,
-    updatedAt: new Date()
-  };
-  const response = await mongodb
-    .getDatabase()
-    .db()
-    .collection('books')
-    .replaceOne({ _id: userId }, book);
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the book.');
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json('Must use a valid book id to update a book.');
+    }
+
+    const bookId = new ObjectId(req.params.id);
+    const book = {
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      publicationDate: req.body.publicationDate,
+      isbn: req.body.isbn,
+      summary: req.body.summary,
+      updatedAt: new Date()
+    };
+
+    const response = await mongodb
+      .getDatabase()
+      .db()
+      .collection('books')
+      .replaceOne({ _id: bookId }, book);
+
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while updating the book.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
 // Delete a single book
 const deleteBook = async (req, res) => {
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Must use a valid book id to delete a book.');
-  }
-  const bookId = new ObjectId(req.params.id);
-  const response = await mongodb.getDatabase().db().collection('books').deleteOne({ _id: bookId });
-  console.log(response);
-  if (response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the book.');
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json('Must use a valid book id to delete a book.');
+    }
+    
+    const bookId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('books').deleteOne({ _id: bookId });
+
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while deleting the book.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
